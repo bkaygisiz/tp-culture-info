822b6f16a91c84cd1d1374e45dfd72238b030fa8
bkaygisiz <81529235+bkaygisiz@users.noreply.github.com>
2024-11-07 15:18:41 +0100

    Delete back-end/out directory

--- back-end/out/interface/routes/index.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const MessageRoutes_1 = require("./message/MessageRoutes");
-exports.router = (0, express_1.Router)();
-exports.router.use('/message', MessageRoutes_1.messageRouter);
